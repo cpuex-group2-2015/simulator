ef293242f2defd3ab658c2356ac0643b98e30166
Koki Kato <ajalabox1@gmail.com>
2015-10-07 12:42:29 +0900

    support comment for test data

--- script/txt2bin.py
+++ script/txt2bin.py
@@ -4,11 +4,6 @@
 import sys
 import re
 
-def flip(l):
-    for i in range(0, len(l), 2):
-        yield l[i + 1]
-        yield l[i]
-
 def chunks(l, n):
     for i in range(0, len(l), n):
         yield l[i:i+n]
@@ -16,8 +11,11 @@ def chunks(l, n):
 if __name__ == '__main__':
     src = open(sys.argv[1], "r")
     dst = open(sys.argv[2], "wb")
+    re_comment = re.compile(r";.+$")
+    re_space   = re.compile(r"\s")
     for line in open(sys.argv[1]):
-        s = re.sub(r"\D", "", line)
+        s = re_space.sub("", re_comment.sub("", line))
+        print(s)
         a = ([int(b, 2) for b in chunks(s, 8)])
         ba = bytearray(a)
         dst.write(ba)
